58667ee9f7304b045c0b51841554fad9a9fadbd2
chamashi <chamashifernando@gmail.com>
2023-03-30 19:42:13 +0530

    customer service class added

---
+++ src/main/java/com/example/restapp/service/CustomerService.java
@@ -0,0 +1,27 @@
+package com.example.restapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+	@Autowired
+	private CustomerRepository repo;
+	
+	public List<Customer>listAll() {		
+		return repo.findAll();
+	}
+	
+	public void save(Customer customer) {
+		repo.save(customer);
+	}
+	
+	public Customer get(Long id) {
+		return repo.findById(id).get();
+	}
+	
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}
+}
+
