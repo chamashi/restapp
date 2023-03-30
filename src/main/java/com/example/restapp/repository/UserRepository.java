afbe52a04d9297de479776d24618b08440f7250d
chamashi <chamashifernando@gmail.com>
2023-03-30 14:20:30 +0530

    error fixed and updated user repo

--- src/main/java/com/example/restapp/repository/UserRepository.java
+++ src/main/java/com/example/restapp/repository/UserRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import com.example.restapp.entity.*;
+import com.example.restapp.entity.User;
+
 public interface UserRepository extends CrudRepository<User, Long> {
 	 
     @Query("SELECT u FROM User u WHERE u.username = :username")
