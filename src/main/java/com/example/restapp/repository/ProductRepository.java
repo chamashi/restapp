859bde8f4fbba9922b2bcec58a10ca92361960d5
chamashi <chamashifernando@gmail.com>
2023-03-30 14:13:58 +0530

    product repo added

---
+++ src/main/java/com/example/restapp/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.restapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.restapp.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
