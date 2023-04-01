4c14f0c881c5b505ce29d30acc168c992a250be9
chamashi <chamashifernando@gmail.com>
2023-04-01 12:20:26 +0530

    product repo updated

--- src/main/java/com/example/restapp/repository/ProductRepository.java
+++ src/main/java/com/example/restapp/repository/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.example.restapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.example.restapp.entity.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
